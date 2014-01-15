a4b5f6a7391f555e3550bb2fcaafd767708c1f6e
Janne <jrho@server.none>
2014-01-15 19:06:41 +0200

    kansiorakenne, projekti ja dokumentit luotu

---
+++ Palikkapeli/src/test/java/palikkapeli/MainTest.java
@@ -0,0 +1,25 @@
+package palikkapeli;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Placeholder
+ *
+ * @author Janne Ruoho
+ */
+public class MainTest {
+
+    public MainTest() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
